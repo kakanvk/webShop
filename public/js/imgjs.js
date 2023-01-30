d94e526d0533ba8f31a5a47b45d88b24345c0f61
Ka Ka <93561031+kakanvk@users.noreply.github.com>
2023-01-30 19:08:56 +0700

    Add files via upload

---
+++ public/js/imgjs.js
@@ -0,0 +1,1 @@
+document.getElementById('images').style.display = 'none';
---
+++ public/js/js.js
@@ -0,0 +1,15 @@
+var modal = document.getElementById('id01');
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+var modal = document.getElementById('id02');
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+$(function () {
+    $('#mi-slider').catslider();
+});
